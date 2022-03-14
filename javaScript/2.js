7f1bb85411cb76d7e0da919036cc4b3069d154ed
backdevhong <ghddls0704@kakao.com>
2022-03-14 17:02:15 +0900

    project

---
+++ javaScript/2.js
@@ -0,0 +1,16 @@
+function a(){
+    return 'a';
+}
+var b;
+var c;
+console.log(a())
+console.log(b())
+console.log(c())
+
+b = function bb(){
+    return 'bb';
+}
+
+c = function() {
+    return 'c';
+}
