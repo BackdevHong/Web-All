8a6c27b7c58f67412eceb15e14bb49a6c9cb1339
HongInSung <ghddls0704@kakao.com>
2022-05-31 15:48:20 +0900

    히끅

---
+++ chap2 - JS/src/funcapproach.js
@@ -0,0 +1,65 @@
+// @ts-check
+// eslint-disable no-restricted-syntax
+
+/**
+ * @typedef Person
+ *
+ * @property {number} age
+ * @property {string} city
+ * @property {string | string[]} [pet]
+ **/
+
+/**@type {Person[]} */
+const people = [
+	{
+		age: 20,
+		city: "서울",
+		pet: ["cat", "dog"],
+	},
+	{
+		age: 40,
+		city: "부산",
+	},
+	{
+		age: 31,
+		city: "대구",
+		pet: ["cat", "dog"],
+	},
+	{
+		age: 36,
+		city: "서울",
+	},
+	{
+		age: 27,
+		city: "부산",
+		pet: "cat",
+	},
+	{
+		age: 24,
+		city: "서울",
+		pet: "dog",
+	},
+];
+
+function solveA() {
+	/** @type {string[]} */
+	const cities = [];
+	for (const person of people) {
+		if (person.age < 30) {
+			if (!cities.find((city) => person.city === city)) {
+				cities.push(person.city);
+			}
+		}
+	}
+	return cities;
+}
+
+function solveAModern() {
+	const allCities = people
+		.filter((person) => person.age < 30)
+		.map((person) => person.city);
+	const set = new Set(allCities);
+	return Array.from(set);
+}
+console.log("SolveA", solveA());
+console.log("SolveAModern", solveAModern());
