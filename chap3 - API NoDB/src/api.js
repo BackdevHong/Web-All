14e27b8be4e8ec316bdd9980308017da893f1b1d
HongInSung <ghddls0704@kakao.com>
2022-06-06 17:05:52 +0900

    api 리펙토링 진행중

--- chap3 - API NoDB/src/api.js
+++ chap3 - API NoDB/src/api.js
@@ -1,1 +1,67 @@
- 
+// @ts-check
+
+/**
+ * @typedef Post
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
+ */
+
+/** @type {Post[]} */
+const posts = [
+    {
+        id: "my_first_project",
+        title: "My first Project",
+        content: "My first Project",
+    },
+    {
+        id: "my_second_project",
+        title: "내 두번째 포스트지롱",
+        content: "My_second_Project",
+    },
+];
+
+/**
+ * @typedef APIResponse
+ * @property {number} statusCode
+ * @property {string | Object} body
+ */
+
+/**
+ * @typedef Route
+ * @property {RegExp} url
+ * @property {'GET' | 'POST'} method
+ * @property {() => Promise<*>} callback
+ */
+
+/** @type {Route[]} */
+const routes = [
+    {
+        url: /^\/posts$/,
+        method: "GET",
+        callback: async () => ({
+            statusCode: 200,
+            body: "All posts",
+        }),
+    },
+    {
+        url: /^\/posts\/([a-zA-z0-9-_]+)$/,
+        method: "GET",
+        callback: async () => ({
+            statusCode: 200,
+            body: {},
+        }),
+    },
+    {
+        url: /^\/posts$/,
+        method: "POST",
+        callback: async () => ({
+            statusCode: 200,
+            body: {},
+        }),
+    },
+];
+
+module.exports = {
+    routes,
+};
--- chap3 - API NoDB/src/main.js
+++ chap3 - API NoDB/src/main.js
@@ -1,81 +1,41 @@
+/* eslint-disable no-console */
+/* eslint-disable no-shadow */
 // @ts-check
-const http = require('http');
+const http = require("http");
 
-/**
- * @typedef Post
- * @property {string} id
- * @property {string} title
- * @property {string} content
- */
-
-/**@type {Post[]} */
-const posts = [
-    {
-        id: 'my_first_project',
-        title: "My first Project",
-        content: "My first Project"
-    },
-    {
-        id: 'my_second_project',
-        title: "내 두번째 포스트지롱",
-        content: "My_second_Project"
-    }
-]
+const { routes } = require("./api");
 
 const server = http.createServer((req, res) => {
-    const POST_ID_REGEX = /^\/posts\/([a-zA-z0-9-_]+)$/
-    const postIdRegexResult = req.url && POST_ID_REGEX.exec(req.url) || undefined;
+    async function main() {
+        const route = routes.find(
+            (route) =>
+                req.url &&
+                req.method &&
+                route.url.test(req.url) &&
+                route.method === req.method
+        );
 
-    if (req.url == "/posts" && req.method === "GET") {
-        const result = {
-            posts : posts.map((post) => ({
-                id: post.id,
-                title: post.title,
-            })),
-            totalCount: posts.length
-        }
-        res.statusCode = 200
-        res.setHeader('Content-type', 'application/json; charset=utf-8')
-        res.end(JSON.stringify(result))
-    } else if (postIdRegexResult) {
-        // GET /posts/:id
-        const postId = postIdRegexResult[1]
-        const post = posts.find(post => post.id === postId)
-        if (post) {
-            res.statusCode = 200;
-            res.setHeader('Content-type', 'application/json; charset=utf-8')
-            res.end(JSON.stringify(post))
-        } else {
+        if (!route) {
             res.statusCode = 404;
-            res.end('Post Not Found')
+            res.end("error, not found");
+            return;
         }
-    } else if (req.url === "/posts" && req.method === 'POST') {
-        req.setEncoding('utf-8')
-        req.on('data', data => {
-            /** 
-             * @typedef CreatePostBody 
-             * @property {string} title
-             * @property {string} content
-             * */
-            /** @type {CreatePostBody} */
-            const body = JSON.parse(data)
-            console.log(body);
-            posts.push({
-                id: body.title.toLowerCase().replace(/\s/g, '_'),
-                title: body.title,
-                content: body.content
-            })
-        })
-        res.statusCode = 200
-        res.end('creating Post')
-    } else {
-        res.statusCode = 404;
-        res.end("Error! 404...")
-    }
-})
 
-const PORT = 4000
+        const result = await route.callback();
+        res.statusCode = result.statusCode;
+        if (typeof result.body === "string") {
+            res.end(result.body);
+        } else {
+            res.setHeader("Content-Type", "application/json; charset=utf-8");
+            res.end(JSON.stringify(result.body));
+        }
+    }
+
+    main();
+});
+
+const PORT = 4000;
 
 server.listen(PORT, () => {
     console.log(`The Server is listening at port: ${PORT}`);
-})
+});
