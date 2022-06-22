83c76091faf5985c228a3b4f7508fca95fe79ecf
backdevhong <ghddls0704@kakao.com>
2022-06-22 19:50:54 +0900

    히히 끝

---
+++ JS-With-CodingApple/level 3/2020 전국대회 JS연습/script/script.js
@@ -0,0 +1,28 @@
+const template = (data) => {
+	return `
+    <div class="card" style="width: 14rem">
+		<img class="card-img-top" src="./필요한파일/${data.photo}" all="Card Image" />
+		<div class="card-body">
+			<h5 class="card-title">${data.title}</h5>
+			<h6 class="card-subtitle mb-2 text-muted">${data.brand}</h6>
+			<h5 class="card-subtitle mb-2 text-muted">${data.price}</h5>
+		</div>
+		<button class="btn btn-light cart">담기</button>
+    </div>
+`;
+};
+
+$(document).ready(() => {
+	$.ajax({
+		url: "./필요한파일/store.json",
+		dataType: "json",
+		method: "GET",
+		success: (data) => {
+			for (i = 0; i < data.products.length; i++) {
+				$(".list").append(template(data.products[i]));
+			}
+		}
+	})
+
+});
+
