341f4fabd9016324109a0984903502b385a6452c
HongInSung <ghddls0704@kakao.com>
2022-06-22 16:36:31 +0900

    히히 아직 계속된다능 히히

--- JS-With-CodingApple/level 3/2020 전국대회 JS연습/script.js
+++ JS-With-CodingApple/level 3/2020 전국대회 JS연습/script.js
@@ -1,13 +1,13 @@
 const template = (data) => {
-	`
+	return `
     <div class="card" style="width: 14rem">
-    <img class="card-img-top" src="./${data.photo}" all="Card Image" />
+    <img class="card-img-top" src="./필요한파일/${data.photo}" all="Card Image" />
     <div class="card-body">
         <h5 class="card-title">${data.title}</h5>
         <h6 class="card-subtitle mb-2 text-muted">${data.brand}</h6>
         <h5 class="card-subtitle mb-2 text-muted">${data.price}</h5>
     </div>
-    </div>;
+    </div>
 `;
 };
 
@@ -15,11 +15,11 @@ $(document).ready(() => {
 	$.ajax({
 		url: "./필요한파일/store.json",
 		dataType: "json",
+		method: "GET",
 		success: (data) => {
-			console.log(data);
-            for(let i in data) {
-                
-            }
+			for (i = 0; i < data.products.length; i++) {
+				$(".list").append(template(data.products[i]));
+			}
 		},
 	});
 });
