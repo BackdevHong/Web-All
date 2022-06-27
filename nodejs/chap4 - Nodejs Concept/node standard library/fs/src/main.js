0e98da26bf7dfda64c473b7e8ff3d565ec6fecf0
HongInSung <ghddls0704@kakao.com>
2022-06-27 13:39:49 +0900

    refactor : main.js refectoring
    main.js 파일을 리펙토링 했습니다.
    추가로 오류도 해결함 ^ㅇ^

--- nodejs/chap4 - Nodejs Concept/node standard library/fs/src/main.js
+++ nodejs/chap4 - Nodejs Concept/node standard library/fs/src/main.js
@@ -1,13 +1,40 @@
+/* eslint-disable node/no-unsupported-features/node-builtins */
 // @ts-check
 
+const log = require("console");
 const fs = require("fs");
 
+// callback-style
 fs.readFile("main.js", "utf-8", (err, result) => {
     if (err) {
-        console.error(err);
+        log.error(err);
     } else {
-        console.log(result);
+        log.log(result);
     }
 });
 
-// console.log("heh");
+// sync-style
+const result = fs.readFileSync("main.js", "utf-8");
+log.log(result);
+
+// 자동으로 오류를 띄워줌, 오류 핸들링을 하기 위해선 TryCatch문 사용
+// const result = fs.readFileSync("main.jsx", "utf-8");
+// console.log(result);
+
+// promise-style
+async function main() {
+    const result2 = await fs.promises.readFile("main.js", "utf-8");
+    log.log(result2);
+}
+
+main();
+
+// 만약 node.js가 구버전이라 fs.promises를 사용하실 수 없다면?
+
+// const util = require("util");
+// async function submain() {
+//     const result = await util.promisify(fs.readFile)("main.js", "utf-8");
+//     console.log(result);
+// }
+
+// submain();
