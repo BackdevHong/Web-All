a15849d750162876bedc67b429040dc63e2f5988
HongInSung <ghddls0704@kakao.com>
2022-06-28 14:01:27 +0900

    refactor: Add chunkCount, Encoding
    청크카운트와 인코딩을 추가하였습니다!

--- nodejs/chap5 - stream/big-file-stream/src/read-file-stream.js
+++ nodejs/chap5 - stream/big-file-stream/src/read-file-stream.js
@@ -3,12 +3,18 @@
 const { log } = require("console");
 const fs = require("fs");
 
-const rs = fs.createReadStream("local/big-file.txt");
+const rs = fs.createReadStream("local/big-file.txt", {
+    encoding: "utf-8",
+});
+
+let chunkCount = 0;
 
 rs.on("data", (data) => {
+    chunkCount += 1;
     log("Event: data", data[0]);
 });
 
 rs.on("end", () => {
     log("Event: end");
+    log("chunkCount: ", chunkCount);
 });
